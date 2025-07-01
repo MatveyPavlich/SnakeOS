3b0ca3f4cb0040bec05e7eea5818db7770984a01
Matvey Pavlich <matveypavlich@gmail.com>
2025-07-01 18:45:20 +0100

    refactor: add comments to bootloader code

--- src/bootloader/main.asm
+++ src/bootloader/main.asm
@@ -26,14 +26,13 @@ ebr_volume_label:           DB 'MatthewOS  '  ; Must be exactly 11 bytes
 ebr_system_id:              DB 'FAT12   '     ; Must be exactly 8 bytes
 
 
-; Initialise data, extra and stack segments
 main:
-    MOV ax, 0x0000      ; Get 0 into ax to later use as data, code and extra segment
-    MOV ds, ax          ; Use first data segment
-    MOV es, ax          ; Use first extra segment
-    MOV ss, ax          ; Use first stack segment
+    MOV ax, 0x0000      ; Get 0 into ax to later use as memory segment number
+    MOV ds, ax          ; Pick first memory segment for data
+    MOV es, ax          ; Pick first memory segment for extra segment also
+    MOV ss, ax          ; Pick first memory segment for stack segment also
 
-    MOV sp, 0x7C00
+    MOV sp, 0x7C00      ; Make stack occumy memory above bootloader code
     MOV si, os_boot_msg ; Set string pointer to msg start
     CALL print
     HLT
@@ -42,25 +41,25 @@ halt:
     JMP halt
 
 print:
-    PUSH si
+    PUSH si             ; Preserve 
     PUSH ax
-    PUSH bx
+    PUSH bx             ; Preserve bx and fall trhough to code below
 
 print_loop:
-    LODSB
-    OR al, al
-    JZ done_print
+    LODSB               ; Load DS:SI byte to al, then increment SI
+    OR al, al           ; Hacky way to avoid CMP al, 0x00
+    JZ done_print       ; Finish printing if zero
 
-    MOV ah, 0x0E
-    MOV bh, 0
-    int 0x10
+    MOV ah, 0x0E        ; Set ah to 0x0E to access BIOS teletype print
+    MOV bh, 0           ; Set page number to 0
+    int 0x10            ; Call BIOS interrup
 
     JMP print_loop
 
 done_print:
-    POP bx
-    POP ax
-    POP si
+    POP bx             ; Get bx value from before print loop
+    POP ax             ; Get ax value from before print loop
+    POP si             ; Get si value from before print loop
     RET
 
 os_boot_msg: DB "Welcome to MatthewOS.", 0x0D, 0x0A, 0x00
