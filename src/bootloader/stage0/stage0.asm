11af1ad1cbcc3489a67dc8f09a3c7664c268b7cd
Matvey Pavlich <matveypavlich@gmail.com>
2025-07-18 20:42:59 +0100

    refactor: clean up search_file

--- src/bootloader/stage0/stage0.asm
+++ src/bootloader/stage0/stage0.asm
@@ -9,27 +9,27 @@ org 0x7C00                                    ; For assembler to organise the co
 bits 16                                       ; For assembler to know that should be in 16 bits
 jmp short main                                ; Jump to code in main (<127 bytes down in memo, so use SHORT)
 nop                                           ; Do nothing for 1 CPU cycle
-
-bdb_oem:                   db 'MSWIN4.1'     ; Tells what formatted the disk
-bdb_bytes_per_sector:      dw 512            ; 
-bdb_sectors_per_cluster:   db 1              ; FAT table has offsets to clusters, not sectors! This is why it is important
-bdb_reserved_sectors:      dw 1              ; Number of sectors before FAT sector including boot sector => 1 => no additional sectors
-bdb_fat_count:             db 2              ; Number of fat tables (one will be for a backup)
-bdb_dir_entries_count:     dw 0xE0           ; Max number of enteries in a root directory
-bdb_total_sectors:         dw 2880
-bdb_media_descriptor_type: db 0xF0           ; Tells 
-bdb_sectors_per_fat:       dw 9              ; Each FAT table will take 9 sectors
-bdb_sectors_per_track:     dw 18             ; Each track will have 18 sectors (ignore the fact that outer tracks are longer)
-bdb_heads:                 dw 2              ; Since we are on the floppy disk we will have 2 heads only
-bdb_hidden_sectors:        dd 0                
-bdb_large_sector_count:    dd 0
-
-ebr_drive_number:          db 0x00           ; Hardcoding that we are using a floppy disk
-                           db 0              ; Reserved, often used for "current head"
-ebr_signature:             db 0x29
-ebr_volume_id:             db 12h,34h,56h,78h
-ebr_volume_label:          db 'SnakeOS    '  ; Must be exactly 11 bytes
-ebr_system_id:             db 'FAT12   '     ; Magical value that tells it is FAT12 (must be 8 bytes)
+ 
+bdb_oem:                   db 'MSWIN4.1'      ; Tells what formatted the disk
+bdb_bytes_per_sector:      dw 512             ; 
+bdb_sectors_per_cluster:   db 1               ; FAT table has offsets to clusters, not sectors! This is why it is important
+bdb_reserved_sectors:      dw 1               ; Number of sectors before FAT sector including boot sector => 1 => no additional sectors
+bdb_fat_count:             db 2               ; Number of fat tables (one will be for a backup)
+bdb_dir_entries_count:     dw 0xE0            ; Max number of enteries in a root directory
+bdb_total_sectors:         dw 2880 
+bdb_media_descriptor_type: db 0xF0            ; Tells 
+bdb_sectors_per_fat:       dw 9               ; Each FAT table will take 9 sectors
+bdb_sectors_per_track:     dw 18              ; Each track will have 18 sectors (ignore the fact that outer tracks are longer)
+bdb_heads:                 dw 2               ; Since we are on the floppy disk we will have 2 heads only
+bdb_hidden_sectors:        dd 0                 
+bdb_large_sector_count:    dd 0 
+ 
+ebr_drive_number:          db 0x00            ; Hardcoding that we are using a floppy disk
+                           db 0               ; Reserved, often used for "current head"
+ebr_signature:             db 0x29 
+ebr_volume_id:             db 12h,34h,56h,78h 
+ebr_volume_label:          db 'SnakeOS    '   ; Must be exactly 11 bytes
+ebr_system_id:             db 'FAT12   '      ; Magical value that tells it is FAT12 (must be 8 bytes)
 
 main:
     
@@ -42,6 +42,7 @@ main:
     call load_root_dir
 
     ; Search for the kernel file
+    xor bx,bx                                 ; Clean bx from address where you dumped root directory
     mov si, file_kernel                       ; Move kernel file name into SI
     mov di, buffer                            ; Set DI to address of loaded root directory
     call search_file                          ; SI = file name, DI = start of root dir (buffer), BX = 0 for a loop
@@ -50,7 +51,7 @@ main:
     ; Search for the stage1 file
     xor bx, bx                                ; Clean bx for the search_file function
     mov di, buffer                            ; Move starting location of the root directory into di
-    mov si, file_stage1                      ; Move stage1 bin file name into si
+    mov si, file_stage1                       ; Move stage1 bin file name into si
     call search_file                          ; SI = file name, DI = start of root dir (buffer), BX = 0 for a loop
     mov [stage1_cluster], ax                  ; Save starting kernel cluster into memory (will be used later)
 
--- src/bootloader/stage0/utils/utils.asm
+++ src/bootloader/stage0/utils/utils.asm
@@ -2,6 +2,8 @@
 ; Input: void
 ; Output:
 ;   - Root directory will be loaded to 0x7E00 (i.e., address of the buffer label)
+;   - Clobbers: AX, BX, CX (might not be an exhaustive list)
+;======================================================================================
 
 load_root_dir:
     
@@ -27,46 +29,49 @@ load_root_dir:
     mov bx, buffer                            ; ES:BX is where root dir will be dumped
     call disk_read                            ; AH = sectors to read; AL - LBA; ES:BX - memo to dump
 
-    xor bx,bx                                 ; Clean bx from address where you dumped root directory
-
 .done:
     ret
 
 
 
-;===============================| Search file in root dir |=============================== 
+;===============================| Search file record in the root directory |=============================== 
 ; Input:
-; - SI = address of a file name (e.g., "STAGE1  BIN")
-; - DI = addres of the root directory (e.g. , 0x7E00)
-; - BX = 0 which will be used as a loop counter (i.e. comparing to bdb_dir_entries_count)
+;   - SI = address of a file name (e.g., "STAGE1  BIN")
+;   - DI = addres of the root directory (e.g. , 0x7E00)
+;   - BX = 0 which will be used as a loop counter (i.e. comparing to bdb_dir_entries_count)
 ;
-; Output:
-; - AX = starting file cluster
+; Output (success):
+;   - AX = first file cluster (e.g., 0x02) of the file
+;   - Clobbers: SI, DI (might not be an exhaustive list)
+;
+; Output (fail)
+;   - System halted since either stage1 or kernel not found
+;=========================================================================================================
 
 search_file:
-    push di                                   ; Preserve di since cmpsb auto incremetns both (si & di)
-    push si
-    mov cx, 11                                ; Set comparison counter to 11 bytes (filename (8 bytes) + file format (3 bytes))
-    REPE CMPSB                                ; Compare exactly all 11 bytes at si:di
-    pop si
-    pop di                                    ; Restore original di
-    je .foundStage1                            ; ZF = 1 if a match is found. di will contain address of first character in the name
+    
+    ; Compare root dirctory record to the file name
+    push di                                   ; Preserve offset to root dir record before cmpsb auto incremetns it
+    push si                                   ; Preserve file name address before cmpsb auto incremetns it
+    mov cx, 11                                ; Set comparison counter to 11 bytes (8 for filename + 3 for file format)
+    repe cmpsb                                ; Compare all 11 bytes at si:di
+    pop si                                    ; Retrieve original file name address
+    pop di                                    ; Retrieve original root dir entery offset
+    je .file_found                            ; ZF = 1 on success. DI will contain address of the first file name character
 
+    ; Move to the next root direcotory record
     add di, 32                                ; Go to next record in root folder (+32 bytes) 
     inc bx                                    ; Save the number of records that were searched 
-    cmp bx, [bdb_dir_entries_count]           ; If all record search then print that kernel wasn't found
-    jl search_file
-    jmp .stage1NotFound
+    cmp bx, [bdb_dir_entries_count]           ; Compare to the maximum number of records possible for a root directory
+    jl search_file                            ; Drop through to file_not_found if all records searched. Otherwise continue
 
-.stage1NotFound:
-    mov si, msg_file_not_found
+.file_not_found:
+    mov si, msg_file_not_found                
     call print
-    jmp halt
+    jmp halt                                  ; Halt. Boot is not possible since either stage1 or kernel not found
 
-.foundStage1:
-    
-    ; Save starting stage1 cluster found from root directory
+.file_found:
     mov si, msg_file_found
     call print
-    mov ax, [di+26]                ; Get first logical cluster (LBA for the cluster)
+    mov ax, [di+26]                           ; Save starting cluster of the file
     ret
