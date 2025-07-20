38276580e45be533c9bef2c20faf86ecb9402b40
Matvey Pavlich <matveypavlich@gmail.com>
2025-07-20 17:40:37 +0100

    feat: document possible debugging options

--- trial_bootloader/.gdbinit
+++ trial_bootloader/.gdbinit
@@ -1,7 +1,3 @@
-# Start in real mode
-# set architecture i386:x86-64 # For long mode debugging
-# set architecture i8086       # For real mode debugging
-# For protected mode debugging
 set architecture i386        
 set disassembly-flavor intel
 target remote localhost:1234
--- trial_bootloader/debug.sh
+++ trial_bootloader/debug.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 PROJECT_DIR=~/repos/SnakeOS/trial_bootloader
-# QEMU_BIN=~/qemu-patched/bin/qemu-system-i386 # For real mode debugging
-QEMU_BIN=qemu-system-i386                      # For protected mode debuggin (not patched, meaning gdb will be in 32 bits)
-# QEMU_BIN=qemu-system-x86_64                      # For long mode (x86_64 architecture)
+# QEMU_BIN=~/qemu-patched/bin/qemu-system-i386 # For real mode debugging (make sure to adjust gdb "set architecture" line)
+QEMU_BIN=qemu-system-i386                      # For protected mode debuggin (not patched, meaning no segment:offset)
+# QEMU_BIN=qemu-system-x86_64                  # For long mode (x86_64 architecture)
 
 # Move terminal window and QEMU into workspace 4
 hyprctl dispatch exec "[workspace 4 silent] kitty --title QEMU_WINDOW bash -c 'cd $PROJECT_DIR && $QEMU_BIN -fda ./build/main.img -debugcon stdio -s -S; exec bash'"
@@ -11,3 +11,18 @@ sleep 0.5
 
 # Execute .gdbinit script
 gdb -q -ex "set pagination off" -x "$PROJECT_DIR/.gdbinit"
+
+
+
+# Real mode:
+#   - QEMU_BIN: ~/qemu-patched/bin/qemu-system-i386 (has addresses in segment:offset)
+#   - .gdbinit (start): set architecture i8086
+
+# Protected mode:
+#   - QEMU_BIN: qemu-system-i386 (what addressing would it have? 32-bit instruction set)
+#   - .gdbinit (start): set architecture i386
+
+# Long mode:
+# Protected mode:
+#   - QEMU_BIN: qemu-system-x86_64 (what addressing would it have? 64-bit instruction set)
+#   - .gdbinit (start): set architecture i386:x86-64
