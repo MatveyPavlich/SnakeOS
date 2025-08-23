b6cd04d7323a983d2c720d1c9f4cf81034a76827
Matvey Pavlich <matveypavlich@gmail.com>
2025-08-23 09:18:55 +0100

    refactor: make gdt function and struct names more intuitive

--- src/kernel/impl/gdt.c
+++ src/kernel/impl/gdt.c
@@ -1,31 +1,33 @@
 #include "stddef.h"
 #include "stdint.h"
 #include "gdt.h"
-#define GDT_SIZE 5
+#define GDT_SEGMENT_DESCRIPTOR_COUNT 5
+#define GDT_SYSTEM_DESCRIPTOR_COUNT 1
 
 extern void loadGdtr(GdtMetadata *m);
 
 void gdtInit() {
-    static SegmentDescriptor gdt_entries[GDT_SIZE] __attribute__((aligned(8)));
-
-    gdt_entries[0] = createSegmentDescriptor(0, 0,       0,    0   ); // Null descriptor
-    gdt_entries[1] = createSegmentDescriptor(0, 0xFFFFF, 0x9A, 0x20); // Kernel code segment
-    gdt_entries[2] = createSegmentDescriptor(0, 0xFFFFF, 0x92, 0xC0); // Kernel data segment
-    gdt_entries[3] = createSegmentDescriptor(0, 0xFFFFF, 0xFA, 0x20); // User code segment
-    gdt_entries[4] = createSegmentDescriptor(0, 0xFFFFF, 0xF2, 0xC0); // User data segment
-    // gdt_entries[5] = createSystemDescriptor(5, , , ); // TSS
+    static GdtSegmentDescriptor gdt_segment_entries[GDT_SEGMENT_DESCRIPTOR_COUNT] __attribute__((aligned(8)));
+    // static GdtSystemDescriptor  gdt_system_entries[GDT_SYSTEM_DESCRIPTOR_COUNT]   __attribute__((aligned(8)));
+ 
+    gdt_segment_entries[0] = createGdtSegmentDescriptor(0, 0,       0,    0   ); // Null descriptor
+    gdt_segment_entries[1] = createGdtSegmentDescriptor(0, 0xFFFFF, 0x9A, 0x20); // Kernel code segment
+    gdt_segment_entries[2] = createGdtSegmentDescriptor(0, 0xFFFFF, 0x92, 0xC0); // Kernel data segment
+    gdt_segment_entries[3] = createGdtSegmentDescriptor(0, 0xFFFFF, 0xFA, 0x20); // User code segment
+    gdt_segment_entries[4] = createGdtSegmentDescriptor(0, 0xFFFFF, 0xF2, 0xC0); // User data segment
+    // gdt_segment_entries[5] = createGdtSystemDescriptor(5, , , ); // TSS
 
     GdtMetadata gdt_metadata;
-    gdt_metadata.gdt_pointer = (uintptr_t)gdt_entries;
-    gdt_metadata.gdt_size = (sizeof(SegmentDescriptor) * GDT_SIZE - 1);
+    gdt_metadata.gdt_pointer = (uintptr_t)gdt_segment_entries;
+    gdt_metadata.gdt_size = (sizeof(GdtSegmentDescriptor) * GDT_SEGMENT_DESCRIPTOR_COUNT - 1); // TODO: adjust GDT metadata to include system descriptors
 
     loadGdtr(&gdt_metadata);
 
     return;
 }
 
-SystemDescriptor createSystemDescriptor(uint64_t base, uint32_t limit) {
-    SystemDescriptor d;
+GdtSystemDescriptor createGdtSystemDescriptor(uint64_t base, uint32_t limit) {
+    GdtSystemDescriptor d;
 
     d.limit_low  = limit & 0xFFFF;
     d.base_low   = base & 0xFFFF;
@@ -47,13 +49,13 @@ void GdtError() {
     while (1) __asm__("hlt");
 }
 
-SegmentDescriptor createSegmentDescriptor(uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
+GdtSegmentDescriptor createGdtSegmentDescriptor(uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
     /*
     limit = 20 bits; base = 32 bits; access = 8 btis; flags = 4 bits
     */
     if(limit > 0xFFFFF)
         GdtError();
-    SegmentDescriptor d = {0};
+    GdtSegmentDescriptor d = {0};
 
     d.low_limit = (uint16_t)(limit & 0xFFFFu);
     d.low_base  = (uint16_t)(base & 0xFFFFu);
--- src/kernel/intf/gdt.h
+++ src/kernel/intf/gdt.h
@@ -1,6 +1,7 @@
 #include "stddef.h"
 #include "stdint.h"
 
+// GdtSegmentDescriptor is a GDT table row that describe a segments (8 bytes)
 typedef struct {
     uint16_t low_limit;            // limit[15:0]
     uint16_t low_base;             // base[15:0]
@@ -9,23 +10,25 @@ typedef struct {
     uint8_t  flags_and_high_limit; // Flags(high nibble) | limit[19:16](low nibble)
     uint8_t  high_base;            // base[31:24]
 
-} __attribute__((packed)) SegmentDescriptor;
+} __attribute__((packed)) GdtSegmentDescriptor;
 
-//  System descriptors are extended to 
+// GdtSystemDescriptor is a GDT table row that describe a system segment (16 bytes)
+// Note that they are extended to 16 bytes in long mode => equvalent to x2 segment descriptors
 typedef struct {
-    uint16_t limit_low;     // Limit 0:15
-    uint16_t base_low;      // Base 0:15
-    uint8_t  base_mid;      // Base 16:23
-    uint8_t  access;        // Type(0x9 / 0xB) | P=1 | DPL | S=0
-    uint8_t  gran;          // Limit 16:19 | flags
-    uint8_t  base_high;     // Base 24:31
+    uint16_t limit_low;            // Limit 0:15
+    uint16_t base_low;             // Base 0:15
+    uint8_t  base_mid;             // Base 16:23
+    uint8_t  access;               // Type(0x9 / 0xB) | P=1 | DPL | S=0
+    uint8_t  gran;                 // Limit 16:19 | flags
+    uint8_t  base_high;            // Base 24:31
 
     // extra 8 bytes for 64-bit TSS
-    uint32_t base_upper;    // Base 32:63
-    uint32_t reserved;      // Must be 0
-} __attribute__((packed)) SystemDescriptor;
+    uint32_t base_upper;           // Base 32:63
+    uint32_t reserved;             // Must be 0
+} __attribute__((packed)) GdtSystemDescriptor;
 
 
+// TSS entry
 typedef struct {
     uint32_t reserved0;
     uint64_t rsp0;      // rsp when entering ring 0
@@ -56,5 +59,5 @@ typedef struct {
 } __attribute__((packed)) GdtMetadata;
 
 void gdtInit();
-SegmentDescriptor createSegmentDescriptor(uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
-SystemDescriptor createSystemDescriptor(uint64_t base, uint32_t limit);
+GdtSegmentDescriptor createGdtSegmentDescriptor(uint32_t base, uint32_t limit, uint8_t access, uint8_t flags);
+GdtSystemDescriptor createGdtSystemDescriptor(uint64_t base, uint32_t limit);
