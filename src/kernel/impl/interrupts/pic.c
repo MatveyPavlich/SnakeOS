b3e26512c838bd68cfaa9bedfb5b760c3e35c60d
MatveyPavlich <matveypavlich@gmail.com>
2025-08-23 18:23:26 +0100

    Add PIC initialization

---
+++ src/kernel/impl/interrupts/pic.c
@@ -0,0 +1,33 @@
+#include "stdint.h"
+#include "ports.h"
+
+#define PIC1_CMD   0x20
+#define PIC1_DATA  0x21
+#define PIC2_CMD   0xA0
+#define PIC2_DATA  0xA1
+
+#define ICW1_INIT  0x10
+#define ICW1_ICW4  0x01
+#define ICW4_8086  0x01
+
+void pic_init(void) {
+    // Start initialization sequence (cascade mode, expect ICW4)
+    outb(PIC1_CMD, ICW1_INIT | ICW1_ICW4);
+    outb(PIC2_CMD, ICW1_INIT | ICW1_ICW4);
+
+    // Remap offsets: master to 0x20 (32), slave to 0x28 (40)
+    outb(PIC1_DATA, 0x20);
+    outb(PIC2_DATA, 0x28);
+
+    // Setup cascading
+    outb(PIC1_DATA, 0x04); // Tell master that slave is at IRQ2
+    outb(PIC2_DATA, 0x02); // Tell slave its cascade identity
+
+    // Environment info
+    outb(PIC1_DATA, ICW4_8086);
+    outb(PIC2_DATA, ICW4_8086);
+
+    // Mask all IRQs except IRQ0 (timer)
+    outb(PIC1_DATA, 0xFE);
+    outb(PIC2_DATA, 0xFF);
+}
