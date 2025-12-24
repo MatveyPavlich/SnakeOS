21fdf2cd8c236cc09e34c66fccf27e3d45254882
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-24 09:02:25 +0000

    refactor: correctly align kmain.c

--- src/kernel/impl/kmain.c
+++ src/kernel/impl/kmain.c
@@ -1,7 +1,7 @@
 // main.c
 /*
-    With -m64 gcc flag, function arguments are as follows:
-    (1) RDI (2) RSI (3) RDX (4) RCX (5) R8 (6) R9
+   With -m64 gcc flag, function arguments are as follows:
+   (1) RDI (2) RSI (3) RDX (4) RCX (5) R8 (6) R9
 
 */
 
@@ -16,32 +16,32 @@ extern char get_char(void);
 
 void start_shell()
 {
-    while(1) {
-        kprintf("SnakeOS> ");
-        
-        char c;
-        do {c = get_char();} while (c != '\n');
-        kprintf("\n");   
-    }
+        while(1) {
+                kprintf("SnakeOS> ");
+
+                char c;
+                do {c = get_char();} while (c != '\n');
+                kprintf("\n");   
+        }
 }
 
 void kmain()
 {
-    print_64_bits("Hello from C!\0");
-    print_clear();
-    kprintf("This is my string\n");
-    kprintf("Hello world!\n");
+        print_64_bits("Hello from C!\0");
+        print_clear();
+        kprintf("This is my string\n");
+        kprintf("Hello world!\n");
 
-    gdtInit();
-    idtInit();
-    kprintf("Interrupts are enabled!!!\n");
+        gdtInit();
+        idtInit();
+        kprintf("Interrupts are enabled!!!\n");
 
-    print_ram_map();
-    start_shell();
-        
-    // Division by zero interrup check
-    // int a = 1, b = 0, c;
-    // c = 1/0;
+        print_ram_map();
+        start_shell();
 
-    while (1) __asm__("hlt");
+        // Division by zero interrup check
+        // int a = 1, b = 0, c;
+        // c = 1/0;
+
+        while (1) __asm__("hlt");
 }
