40b0fa45289ae36ed70e885c8f1425a6387ef0d9
Matvey Pavlich <matveypavlich@gmail.com>
2025-12-29 21:32:43 +0000

    refactor: cleanup kmain.c even further

--- init/kmain.c
+++ init/kmain.c
@@ -8,24 +8,24 @@
 #include "tests.h"
 
 extern void print_64_bits(const char* str);
-extern char get_char(void);
 
 void kmain()
 {
-        print_64_bits("Hello from C!\0");
         print_clear();
-        kprintf("This is my string\n");
-        kprintf("Hello world!\n");
 
+        /* Arch init */
         gdt_init();
         idt_init();
         irq_arch_init();
         kprintf("Interrupts are enabled!!!\n");
         __asm__ volatile ("sti"); // Enable interrupts after PIC is set up
+
+        /* Key drevices init */
         print_ram_map();
         timer_init();
         keyboard_init();
 
+        /* Tests for debugging purposes */
         // tests_div_by_zero();
 
         while (1) __asm__("hlt");
