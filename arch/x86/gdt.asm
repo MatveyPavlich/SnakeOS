28799915b99424d62f299ff1a5a010d51cdd14d1
Matvey Pavlich <matveypavlich@gmail.com>
2026-01-14 21:58:07 +0000

    refactor: cleanup gdt.asm

--- arch/x86/gdt.asm
+++ arch/x86/gdt.asm
@@ -1,37 +1,39 @@
-global loadGdtr
-global loadLtr
+global gdt_load
+global gdt_load_tss
 global idt_load
 
 bits 64
 
-; Inputs: 
-;   RDI = pointer to GDT metadata passed thrugh an argument in C
-;   RSI = code segment selector (e.g., 0x08, L=1)
-loadGdtr:
+; void gdt_load(struct gdt_metadata *m) - Function to load GDT into the CPU.
+; @m:                                     GDT metadata struct defined in gdt.c.
+gdt_load:
     
-    ; Load GDT metadata into gdt register (gdtr)
-    lgdt [rdi]
+         ; Load GDT metadata into the gdt register (gdtr)
+        lgdt [rdi]
     
-    ; Select kernel data segment
-    mov ax, 0x10        
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
+        ; Reload memo segments to the kernel data segment
+        mov ax, 0x10        
+        mov ds, ax
+        mov es, ax
+        mov fs, ax
+        mov gs, ax
+        mov ss, ax
 
-    ; Far return into kernel's CS 
-    push 0x8                     ; Changing to 0x18, 0x1B breaks it
-    lea rax, [rel .return]
-    push rax
-    retfq                        ; Do far return
+        ; Far return into kernel's CS 
+        push 0x8                     ; Changing to 0x18, 0x1B breaks it
+        lea rax, [rel .return]
+        push rax
+        retfq                        ; Do far return
 
-    .return:
+        .return:
+                ret
+
+; void gdt_load_tss(uint_16_t      - Function to load the GDT offset for
+;                   gdt_tss_index)   into the CPU.
+; @gdt_tss_index:                    Byte offset for the GDT into the TSS
+;                                    descriptor;
+gdt_load_tss:
+        ltr di
         ret
-
-; void ltr(uint16_t selector);
-loadLtr:
-    ltr di
-    ret
 
 
