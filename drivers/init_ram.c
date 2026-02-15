624384203ee1586d236cdf25e1dfefa318e38227
Matvey Pavlich <matveypavlich@gmail.com>
2026-02-15 23:51:11 +0000

    feat: mmu layer kick-off

--- drivers/init_ram.c
+++ drivers/init_ram.c
@@ -8,11 +8,22 @@ struct ram_entry {
         uint32_t type;
 } __attribute__((packed));
 
+struct memory_boot_info {
+        uint16_t mem_regions_count;
+        (struct ram_entry *) mem_regions_buf;
+} __attribute__((packed));
+
 struct ram_entry *ram_entries_by_bios = (struct ram_entry *)0x86000;
-uint16_t *ram_entries = (uint16_t *)0x85FFD;
+uint16_t *ram_entries = (uint16_t *)0x85FFD; /* Just 2 bytes before actual
+                                                entries */
 
 void print_ram_map(void)
 {
+        struct memory_boot_info boot_info = {
+                .mm_regions_c,
+                .1,
+        };
+
         kprint("Entries count: %d\n", *ram_entries);
         for (int i = 0; i < *ram_entries; i++) {
                 struct ram_entry e = ram_entries_by_bios[i];
